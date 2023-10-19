0536addd8933d32fa282aa35b127ef27699cc5e7
Ayrton Lopes <106751266+Ayrton54@users.noreply.github.com>
2023-10-19 15:50:08 -0300

    Sua mensagem de commit aqui

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "4987", modelo:"Modelo2", imei:"22222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n"+"Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "123456", modelo:"Modelo1", imei:"1111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
